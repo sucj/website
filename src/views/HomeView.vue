7b8d594eb27a598120f5d0b2f4cb406e546046be
KKW557 <kevin557557@gmail.com>
2025-06-26 20:43:52 +0800

    n

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <h1>SUC.ICU</h1>
+
+  <hr>
+  <h2>Fabric服务端生态支持</h2>
+  See GitHub and Modrinth
+
+  <hr>
+  <h2>sucj</h2>
+  这是复杂的
+  <p>mc.suc.icu</p>
+  <p>test.suc.icu</p>
+  <a href="https://qm.qq.com/q/AT40capg6O">QQ</a>
+
+  <hr>
+  <h2>MW78</h2>
+  <a href="https://qm.qq.com/q/AT40capg6O">QQ</a>
+  <br>
+  <a href="https://github.com/sucj/MegaWalls78-Open">GitHub</a>
+  <br>
+  <a href="https://space.bilibili.com/386919430/lists?sid=3482643">BiliBili</a>
 </template>
